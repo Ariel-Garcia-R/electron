2437322dace6f88a3fb8ff0518e30de2db5ce267
Ariel García Rodríguez <arielgarciarodriguez004@gmail.com>
2024-01-27 03:31:32 -0500

    Created card with sliders

--- preload.js
+++ preload.js
@@ -1,14 +1,18 @@
-// preload.js
-
-// All the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
+// Waits for the DOM to load
 window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-      const element = document.getElementById(selector)
-      if (element) element.innerText = text
-    }
-  
-    for (const dependency of ['chrome', 'node', 'electron']) {
-      replaceText(`${dependency}-version`, process.versions[dependency])
-    }
-  })
+
+  // Selecting inputs from index.html
+  const range_1 = document.getElementById("range_1");
+  const range_2 = document.getElementById("range_2");
+  const number_1 = document.getElementById("range_1_number");
+  const number_2 = document.getElementById("range_2_number");
+
+  // Updating the value on input events
+
+  range_1.oninput = () => number_1.value = range_1.value
+  range_2.oninput = () => number_2.value = range_2.value
+  number_1.oninput = () => range_1.value = number_1.value
+  number_2.oninput = () => range_2.value = number_2.value
+
+
+})
